bcc3e438470925afced5de78682ccb81279f0ca0
2HyeonHo <dpqlvkvk@naver.com>
2023-02-23 23:52:01 +0900

    feat : api호출을 통한 코인 종가 dto 생성

---
+++ src/main/java/com/example/coinProject/price/dto/PriceResponse.java
@@ -0,0 +1,16 @@
+package com.example.coinProject.price.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class PriceResponse {
+
+    @JsonProperty("trade_price")
+    BigDecimal price;
+}
