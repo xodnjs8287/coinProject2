d1cc8a4612a7da05b551f606898a94bdafa6c5f1
2HyeonHo <dpqlvkvk@naver.com>
2023-02-23 23:50:10 +0900

    refactor : api호출을 통해 불필요한 코드 삭제

--- src/main/java/com/example/coinProject/coin/repository/PriceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.coinProject.coin.repository;
-
-import com.example.coinProject.coin.domain.Price;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PriceRepository extends JpaRepository<Price, Integer> {
-
-}
