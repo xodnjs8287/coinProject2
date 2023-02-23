2b88531d8f3e16e8871fa7877088ef0231c6b240
2HyeonHo <dpqlvkvk@naver.com>
2023-02-23 23:54:51 +0900

    feat : RSI 값 계산기능 작성

---
+++ src/main/java/com/example/coinProject/price/controller/PriceController.java
@@ -0,0 +1,20 @@
+package com.example.coinProject.price.controller;
+
+import com.example.coinProject.price.service.PriceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/price")
+public class PriceController {
+
+    private final PriceService priceService;
+
+    @GetMapping
+    public Double inquiryRsi() {
+        return priceService.getRsi();
+    }
+}
