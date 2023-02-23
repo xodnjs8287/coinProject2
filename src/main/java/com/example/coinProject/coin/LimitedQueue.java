00bb28b8df20bd5cb5ba0e3ffdb186a7996ae8fd
2HyeonHo <dpqlvkvk@naver.com>
2023-02-23 23:54:20 +0900

    feat : 크기 제한이 있는 Queue구현

---
+++ src/main/java/com/example/coinProject/coin/LimitedQueue.java
@@ -0,0 +1,19 @@
+package com.example.coinProject.coin;
+
+import java.util.LinkedList;
+
+public class LimitedQueue<E> extends LinkedList<E> {
+
+    private int limit;
+
+    public LimitedQueue(int limit) {
+        this.limit = limit;
+    }
+
+    @Override
+    public boolean add(E e) {
+        super.add(e);
+        while (size() > limit) {super.remove();}
+        return true;
+    }
+}
